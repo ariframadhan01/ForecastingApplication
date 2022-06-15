0879b46cf5954aea72f9834f04c7ecf351b81c0c
Muhammad Arif Ramadhani <63437285+ariframadhan01@users.noreply.github.com>
2022-06-15 22:46:43 +0700

    Add Files

---
+++ README.md
@@ -0,0 +1,6 @@
+# aplikasi-forecasting-peramalan-PMB
+Aplikasi Forecasting Peramalan Penerimaan Mahasiswa Baru Dengan PHP
+
+### -
+PHP7
+MySQLi
---
+++ assets/footer.php
@@ -0,0 +1,14 @@
+<html>
+<footer class="site-footer">
+    <div class="footer-inner bg-white">
+        <div class="row">
+            <div class="col-sm-6">
+                Copyright &copy; 2020 Muhammad Arif Ramadhani
+            </div>
+            <div class="col-sm-6 text-right">
+                Support by <a href="https://umk.ac.id/">Universitas Muria Kudus</a>
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
